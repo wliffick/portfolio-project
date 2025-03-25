58bdab060d2eb3a8a05e76f9b52540e3c6a94cf7
wliffick <liffickwade@gmail.com>
2025-03-25 18:32:11 -0400

    update 3

--- src/components/securemessage/SecureMessage.java
+++ src/components/securemessage/SecureMessage.java
@@ -1,4 +1,73 @@
 package components.securemessage;
 
-public class SecureMessage {
+import components.standard.Standard;
+
+/**
+ * Kernel interface for secure communication with primary methods.
+ */
+public interface SecureMessage extends Standard<SecureMessage> {
+
+    /**
+     * Encrypts the given message using the provided key.
+     *
+     * @param message
+     *            the message to be encrypted
+     * @param key
+     *            the encryption key
+     * @return the encrypted message
+     */
+    String encrypt(String message, String key);
+
+    /**
+     * Decrypts the given encrypted message using the provided key.
+     *
+     * @param encryptedMessage
+     *            the encrypted message to be decrypted
+     * @param key
+     *            the encryption key
+     * @return the decrypted message
+     */
+    String decrypt(String encryptedMessage, String key);
+
+    /**
+     * Checks if the given message is encrypted.
+     *
+     * @param message
+     *            the message to be checked
+     * @return true if the message is encrypted, false otherwise
+     */
+    boolean isEncrypted(String message);
+
+    /**
+     * Sets the message to be encrypted or decrypted.
+     *
+     * @param message
+     *            the message to be set
+     */
+    void setMessage(String message);
+
+    /**
+     * Sets the encryption key.
+     *
+     * @param key
+     *            the encryption key to be set
+     */
+    void setKey(String key);
+
+    /**
+     * Retrieves the stored message corresponding to the given encrypted
+     * message.
+     *
+     * @param encryptedMessage
+     *            the encrypted message
+     * @return the corresponding stored decrypted message
+     */
+    String getMessage(String encryptedMessage);
+
+    /**
+     * Retrieves the currently stored encryption key.
+     *
+     * @return the stored encryption key
+     */
+    String getKey();
 }
--- src/components/securemessage/SecureMessageSecondary.java
+++ src/components/securemessage/SecureMessageSecondary.java
@@ -1,18 +1,36 @@
 package components.securemessage;
 
 /**
- * Abstract class for secure communication with secondary methods.
+ *
+ * Abstract class implementing secondary methods for secure communication.
  */
-public abstract class SecureMessageSecondary extends SecureMessage {
+public abstract class SecureMessageSecondary implements SecureMessage {
 
-    @Override
+    /**
+     * Encrypts and prints the message to the console.
+     *
+     * @param message
+     *            the message to be encrypted
+     * @param key
+     *            the encryption key
+     * @param recipient
+     *            the recipient of the encrypted message
+     */
     public void sendEncryptedMessage(String message, String key,
             String recipient) {
         String encryptedMessage = this.encrypt(message, key);
         System.out.println("To " + recipient + ": " + encryptedMessage);
     }
 
-    @Override
+    /**
+     * Verifies the integrity of the message by comparing their hashes.
+     *
+     * @param message
+     *            the message to be verified
+     * @param hash
+     *            the hash to be compared
+     * @return true if the hashes match, false otherwise
+     */
     public boolean verifyHash(String message, String hash) {
         int computedHash = 0;
         for (int i = 0; i < message.length(); i++) {
@@ -21,7 +39,15 @@ public abstract class SecureMessageSecondary extends SecureMessage {
         return String.valueOf(computedHash).equals(hash);
     }
 
-    @Override
+    /**
+     * Hashes the message and encrypts the message using the provided key.
+     *
+     * @param message
+     *            the message to be hashed and encrypted
+     * @param key
+     *            the key to be used for encryption
+     * @return the hashed and encrypted message
+     */
     public String hashAndEncrypt(String message, String key) {
         int hash = 0;
         for (int i = 0; i < message.length(); i++) {
@@ -31,8 +57,26 @@ public abstract class SecureMessageSecondary extends SecureMessage {
         return this.encrypt(hashedMessage, key);
     }
 
-    @Override
-    public String decryptVerifyHash(String encryptedMessage, String key, String hash) {
+    /**
+     * Decrypts the message using the provided key and verifies its integrity by
+     * comparing the hashes.
+     *
+     * @param encryptedMessage
+     *            the encrypted message to be decrypted
+     * @param key
+     *            the encryption key
+     * @param hash
+     *            the hash to be compared
+     * @return the decrypted message if hashes match, otherwise null
+     */
+    public String decryptVerifyHash(String encryptedMessage, String key,
+            String hash) {
         String decryptedMessage = this.decrypt(encryptedMessage, key);
-        return this.verifyHash(decryptedMessage, hash) ? decryptedMessage : "Hash verification failed";
+        String result = "Hash verification failed";
+        if (this.verifyHash(decryptedMessage, hash)) {
+            result = decryptedMessage;
+        }
+        return result;
     }
+
+}
